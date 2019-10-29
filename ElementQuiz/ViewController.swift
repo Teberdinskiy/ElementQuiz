58b8eb3659bdc77c140ac4a5935779a83990255f
Alikhan Batchaev <loko-han@mail.ru>
2019-10-29 05:26:05 +0300

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Алихан Батчаев on 29.10.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
